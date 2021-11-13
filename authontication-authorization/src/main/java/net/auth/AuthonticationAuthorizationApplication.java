49887dc70bb8d6d9f02407c2eb5c1de9d253e6a4
xavioo <rucahoxavi@gmail.com>
2021-11-13 06:46:09 +0200

    Start a project

---
+++ authontication-authorization/src/main/java/net/auth/AuthonticationAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package net.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthonticationAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthonticationAuthorizationApplication.class, args);
+	}
+
+}
---
+++ authontication-authorization/src/test/java/net/auth/AuthonticationAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package net.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthonticationAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
